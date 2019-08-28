bf7425900881168fde5e5d98f79dde990273698b
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-08-28 10:15:44 -0400

    added MONGO_URI

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ require("./routes/html-api-routes.js")(app);
 
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB, { useNewUrlParser: true }, ()=>{
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, ()=>{
   console.log("MongoDB connected...")
 });
 
