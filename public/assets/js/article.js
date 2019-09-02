5f3c75cc07d958934ce98d0a0e7c1930cd63f7e4
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-09-02 11:14:19 -0400

    added background image

--- public/assets/js/article.js
+++ public/assets/js/article.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $('.modal').modal();
     $('.tooltipped').tooltip();
+    $('.parallax').parallax();
     $("#scrape-btn").on("click", function(e){
         window.location.href = "/index";
     })
